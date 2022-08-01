4170ab0c2c3956560c6ff940848a9bcee2e2d507
Mateus Gamba <mateusgamba@gmail.com>
2022-07-31 22:17:49 -0300

    feat: deployment

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
+// use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Passport::loadKeysFrom('/app');
+        //Passport::loadKeysFrom('/app');
     }
 }
