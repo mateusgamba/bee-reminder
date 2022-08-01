7d385f8d598cef92c1c64995b90b37ca345d4ca3
Mateus Gamba <mateusgamba@gmail.com>
2022-07-31 21:39:18 -0300

    feat: create key

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Passport::loadKeysFrom('/app/storage/app');
     }
 }
